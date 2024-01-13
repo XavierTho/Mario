cd5a4b8819bfeee158a1b5df4db04bf90c05a37c
XavierTho <xavierathompson@gmail.com>
2024-01-13 15:23:14 -0800

    added special ability

--- assets/js/platformer2/Goomba.js
+++ assets/js/platformer2/Goomba.js
@@ -12,6 +12,8 @@ export class Goomba extends Character {
         //Access in which a Goomba can travel
         this.minPosition = minPosition * GameEnv.innerWidth;
         this.maxPosition = this.x + xPercentage * GameEnv.innerWidth;
+
+        this.immune = 0;
     }
 
     update() {
@@ -24,6 +26,11 @@ export class Goomba extends Character {
         // Every so often change direction
         if (Math.random() < 0.005) {
             this.speed = Math.random() < 0.5 ? -this.speed : this.speed;
+        }
+
+        if (Math.random() < 0.00001) {
+            this.canvas.style.filter = 'brightness(1000%)';
+            this.immune = 1;
         }
 
         // Move the enemy
@@ -39,8 +46,8 @@ export class Goomba extends Character {
         }
         if (this.collisionData.touchPoints.other.id === "player") {
             // Collision: Top of Goomba with Bottom of Player
-            if (this.collisionData.touchPoints.other.bottom) {
-                console.log("Bye Bye Goomba");
+            if (this.collisionData.touchPoints.other.bottom && this.immune === 0) {
+                // console.log("Bye Bye Goomba");
                 this.x = GameEnv.innerWidth + 1;
                 this.destroy();
             }
